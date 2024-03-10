3ee495cd18e6fb8edac1a4c73f0d0b2dc00e0359
Harshit Shukla <mrharshit2409@gmail.com>
2024-03-10 16:48:00 +0530

    Added navbar and sidebar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/nav-logo.svg'
+import navlogo from '../../assets/logo.png'
 import navprofile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <img src={ navlogo } alt="" className='nav-logo' />
+            <div className="nav-logo">
+                <img src={navlogo} alt="" />
+                <p>Shoplet-admin</p>
+            </div>
             <img src={ navprofile } alt="" className='nav-profile' />
         </div>
     )
